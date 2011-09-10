f7f7d56332a11dc931e19ddf88a32485f7bec01c
squ1r3ll <josh.jacobs19@gmail.com>
2011-09-10 16:47:33 +0100

    tset

--- Form1.vb
+++ Form1.vb
@@ -12,7 +12,7 @@ Public Class Form1
 
     Public Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         userpasssave = ""
-
+        'test
     End Sub
 
     Public Sub cmdAddUser_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAddUser.Click
