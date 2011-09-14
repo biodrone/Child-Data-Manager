2d029590ff43cefa24e7aba82edab1880270a170
squ1r3ll <josh.jacobs19@gmail.com>
2011-09-14 08:36:25 +0100

    MainForm.vb buttons all work. Form complete.

--- MainForm.vb
+++ MainForm.vb
@@ -3,4 +3,19 @@
     Private Sub MainForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub cmdLogout_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLogout.Click
+        Form1.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub cmdAddOld_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAddOld.Click
+        OldChildDataEntry.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub cmdAddNew_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAddNew.Click
+        NewChildDataEntry.Show()
+        Me.Close()
+    End Sub
 End Class
