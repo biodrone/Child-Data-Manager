641b2880785e530419f32ab0c054d04edfd835ca
squ1r3ll <josh.jacobs19@gmail.com>
2011-09-14 08:29:11 +0100

    14/09/2011 - MainForm.vb Design Complete

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -22,8 +22,52 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.cmdAddNew = New System.Windows.Forms.Button()
+        Me.cmdAddOld = New System.Windows.Forms.Button()
+        Me.cmdLogout = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdAddNew
+        '
+        Me.cmdAddNew.Location = New System.Drawing.Point(12, 12)
+        Me.cmdAddNew.Name = "cmdAddNew"
+        Me.cmdAddNew.Size = New System.Drawing.Size(110, 40)
+        Me.cmdAddNew.TabIndex = 0
+        Me.cmdAddNew.Text = "Add A New Child"
+        Me.cmdAddNew.UseVisualStyleBackColor = True
+        '
+        'cmdAddOld
+        '
+        Me.cmdAddOld.Location = New System.Drawing.Point(128, 12)
+        Me.cmdAddOld.Name = "cmdAddOld"
+        Me.cmdAddOld.Size = New System.Drawing.Size(110, 40)
+        Me.cmdAddOld.TabIndex = 1
+        Me.cmdAddOld.Text = "Add Data For An Existing Child"
+        Me.cmdAddOld.UseVisualStyleBackColor = True
+        '
+        'cmdLogout
+        '
+        Me.cmdLogout.Location = New System.Drawing.Point(244, 12)
+        Me.cmdLogout.Name = "cmdLogout"
+        Me.cmdLogout.Size = New System.Drawing.Size(110, 40)
+        Me.cmdLogout.TabIndex = 2
+        Me.cmdLogout.Text = "Log Out"
+        Me.cmdLogout.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "MainForm"
+        Me.ClientSize = New System.Drawing.Size(365, 60)
+        Me.Controls.Add(Me.cmdLogout)
+        Me.Controls.Add(Me.cmdAddOld)
+        Me.Controls.Add(Me.cmdAddNew)
+        Me.Name = "MainForm"
+        Me.Text = "Main Form"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents cmdAddNew As System.Windows.Forms.Button
+    Friend WithEvents cmdAddOld As System.Windows.Forms.Button
+    Friend WithEvents cmdLogout As System.Windows.Forms.Button
 End Class
--- MainForm.vb
+++ MainForm.vb
@@ -1,3 +1,6 @@
 ﻿Public Class MainForm
 
+    Private Sub MainForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
